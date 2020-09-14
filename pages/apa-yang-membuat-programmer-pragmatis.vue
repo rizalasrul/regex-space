1d0c7dbcb69c19d2dbbac64faa60231073545a2a
Rizal Asrul Pambudi <rizal.asrul@live.com>
2020-09-14 16:20:22 +0700

    feat: Add new article /apa-yang-membuat-programmer-pragmatis

---
+++ pages/apa-yang-membuat-programmer-pragmatis.vue
@@ -0,0 +1,164 @@
+<template>
+  <section>
+    <div class="mx-auto pt-24 px-12 py-24">
+      <center>
+        <div>
+          <h1 class="font-semibold text-3xl text-gray-700">Apa yang membuat programmer pragmatis?</h1>
+          <hr class="w-2/5" style="
+            margin-top: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #c3c3c3;
+            ">
+          <p class="text-gray-600 text-lg">03 Februari 2020</p>
+        </div>
+      </center>
+    </div>
+
+    <div class="container mx-auto pb-24 px-12">
+      <article class="pb-12 prose prose-xl" style="max-width: none;">
+        <h2>Makna pragmatic</h2>
+        <p>
+          Sebelum membahas mengenai <i>pragmatic programmer</i>, kita harus mengerti apa yang dimaksud dengan pragmatic. Siapapun bisa <i>googling</i> untuk mencari tahu apa yang dimaksud dengan kata “pragmatic”. Tapi saya mencoba untuk mendemostrasikan apa yang dimaksud dengan pragmatic yang sebenarnya.
+        </p>
+        <p>Anggap saja ada yang bertanya ke kita “Jam berapa sekarang?”.</p>
+        <p>
+          Secara literal, orang tersebut benar-benar menanyakan jam berapa saat ini. Maka tanggapan kita adalah memberitahukan sekarang jam berapa dengan menjawab “sekarang jam tujuh malam”.
+        </p>
+        <p>
+          Akan tetapi secara pragmatis, bisa jadi orang tersebut menanyakan “kenapa kamu terlambat datang?”. Oleh karena itu alih-alih memberitahukan jam berapa saat ini, kita menjelaskan alasan mengapa kita datang terlambat.
+        </p>
+        <p>
+          Atau anggap saja ada yang bertanya ke kita “bisakah kamu memberikan kecap manis itu ke saya?”.
+        </p>
+        <p>
+          Secara literal, orang tersebut menanyakan apakah kita bisa memberikan kecap manis yang ada di depan kita ke dia. Maka tanggapan kita secara literal adalah menjawab “ya” atau “tidak”.
+        </p>
+        <p>
+          Tetapi jika kita bersikap pragmatis, kita paham bahwa yang dimaksud sebenarnya adalah “Maukah kita memberikan kecap manis tersebut ke dia?”. Maka alih-alih kita menjawab “ya” atau “tidak”, maka kita langsung memberikan kecap manis itu ke orang yang bertanya tersebut.
+        </p>
+        <h2>So, what makes a pragmatic programmer?</h2>
+        <figure class="px-64">
+          <img src="~/static/images/blog_thumbnails/apa-yang-membuat-programmer-pragmatis.webp" alt="Photo by Grzegorz Walczak on Unsplash">
+          <figcaption>Photo by <a href="https://unsplash.com/@grzegorzwalczak" target="_blank">Grzegorz Walczak</a> on <a href="https://unsplash.com/photos/yoIIPcrWhjI" target="_blank">Unsplash</a></figcaption>
+        </figure>
+        <p>
+          Setiap developer itu unik. Mereka memiliki kelemahan dan kelebihan masing-masing. Mereka memiliki preferensi dan ketidaksukaan masing-masing. Seiring waktu, mereka menciptakan lingkungan pribadi mereka sendiri.
+        </p>
+        <p>
+          Lingkungan itu akan mencerminkan kepribadian developer sama kuatnya seperti halnya hobi, pakaian, hingga potongan rambutnya. Namun, jika kita seorang programmer yang pragmatis, kita akan terlihat memiliki karakteristik seperti berikut ini:
+        </p>
+        <h3>Early adopter/fast adapter</h3>
+        <p>
+          Kita memiliki naluri terhadap teknologi dan suka mencoba berbagai hal. Ketika diberi sesuatu yang baru, kita dapat menangkapnya dengan cepat dan mengintegrasikannya dengan seluruh wawasan yang kita punya. <i>Our confidence is born of experience</i>.
+        </p>
+        <h3>Memiliki keingintahuan yang sangat tinggi</h3>
+        <p>
+          Kita cenderung bertanya “wow kodemu rapi sekali. bagaimana caramu melakukannya? Apakah kamu memiliki masalah dalam menggunakan library tersebut? Lalu, bagaimana cara implementasinya ya?”. <i>You are a pack rat for little facts, each of which may affect some decision years from now</i>.
+        </p>
+        <h3>Critical thinker</h3>
+        <p>
+          Kita jarang menerima pekerjaan tanpa diberikan maksud dari apa yang akan dikerjakan terlebih dahulu. Ketika kita mendapatkan sebuah pekerjaan, lalu atasan kita berkata “pekerjaan ini harus dilakukan karena akan memberikan solusi ke banyak orang”, mulailah kita mencium pekerjaan tersebut sebagai tantangan.
+        </p>
+        <h3>Realistic</h3>
+        <p>
+          Kita mencoba memahami <i>root cause</i> dari setiap masalah yang kita hadapi. Karakteristik realistis ini memberi kita perasaan yang baik tentang betapa sulitnya berbagai hal, dan berapa lama hal itu akan terjadi. Memahami bahwa suatu proses harus sulit atau akan memakan waktu cukup lama untuk diselesaikan, memberikan kita kekuatan dan perhitungan yang tepat untuk tetap melakukannya.
+        </p>
+        <h3>Orang yang serba tahu (generalis)</h3>
+        <p>
+          Kita berusaha keras untuk terbiasa dengan berbagai teknologi dan lingkungan, dan kita berupaya untuk mengikuti tren teknologi baru. Meskipun pekerjaan kita saat ini mengharuskan untuk menjadi seseorang yang spesialis, kita akan selalu dapat pindah ke bidang dan tantangan baru.
+        </p>
+        <p>
+          Selain keempat karakteristik di atas, ada dua tips dasar yang menjadikan diri kita sebagai programmer pragmatis:
+        </p>
+        <ol>
+          <li>
+            <b>Care about your craft</b>
+            <p style="margin-top: 0px;">
+              Tidak ada gunanya mengembangkan software atau aplikasi, kecuali kita melakukannya dengan kepedulian dan kehati-hatian.
+            </p>
+          </li>
+          <li>
+            <b>Think! about your work</b>
+            <p style="margin-top: 0px;">
+              Alih-alih berpikir untuk menjadi pragmatis programmer, lebih baik berpikir terhadap apa yang kita kerjakan hari ini. Fokus terhadap yang proyek yang kita kerjakan sekarang. <i>Never run on autopilot</i>. Teruslah berpikir, selalu kritis terhadap pekerjaan kita. Secara tidak langsung, kita telah menerapkan mantra programmer pragmatis: THINK.
+            </p>
+          </li>
+        </ol>
+        <h2>Individual yang pragmatis di tim yang besar</h2>
+        <p>
+          Beberapa orang merasa bahwa tidak ada ruang untuk individualitas di tim yang besar atau di proyek yang kompleks. Mereka bilang “<i>software construction is an engineering discipline. Hal itu akan rusak jika salah seorang dari tim tersebut membuat keputusan untuk diri mereka sendiri</i>”.
+        </p>
+        <p>Hal tersebut sangat tidak benar.</p>
+        <p>
+          <i>The construction of software should be an engineering discipline</i>. Namun, ini tidak menghalangi keahlian tiap individu. Coba bayangkan sebuah museum yang dibangun pada saat ratusan tahun yang lalu. Pekerjaan pembangunan tersebut dilakukan puluhan, atau mungkin ratusan orang, yang tersebar di beberapa dekade. Lalu pekerjaan tersebut dilanjutkan ke orang yang berbeda tiap dekadenya. Keilmuwan dalam membangun museum tersebut juga berkembang tiap dekadenya, sehingga pekerja yang melanjutkan pembangunan tersebut memiliki keilmuan yang berkembang. Tiap individu dari pekerja tersebut dapat berkontribusi dengan kemampuan individu yang dia miliki, tanpa harus mengesampingkan kemampuannya sebagai bagian dari tim yang membangun museum tersebut.
+        </p>
+        <h2>Poinnya adalah proses yang berkelanjutan</h2>
+        <blockquote>
+          <p>
+            Seorang turis yang mengunjungi Kebun Raya Bogor bertanya kepada tukang kebun bagaimana dia membuat rumput di taman tersebut begitu indah. “Gampang sekali”, kata tukang kebun tersebut. “Kamu cukup menyiraminya tiap hari, memotongnya setiap sore, dan membersihkan kotoran di sekitar rumput tersebut seminggu sekali”.
+          </p>
+          <p>
+            “Hanya itu?”, kata turis tersebut.
+          </p>
+          <p>
+            “Tentu saja”, jawab tukang kebun dengan santai. “Lakukan sepuluh tahun dan kamu akan memiliki halaman yang bagus”
+          </p>
+        </blockquote>
+        <p>
+          Untuk membuat rumput yang indah di halaman rumah seperti halnya rumput yang adai di Kebun Raya Bogor, membutuhkan cukup sedikit perawatan yang dilakukan setiap hari secara konsisten. Begitu pula dengan dengan programmer yang hebat.
+        </p>
+        <p>
+          Perusahaan konsultan di Jepang sering kali mengucapkan kata kaizen dalam percakapannya. “Kaizen” dalam istilah jepang adalah sebuah konsep terus-menerus melakukan perbaikan kecil. Hal itu dianggap sebagai salah satu alasan utama mengapa industri manufaktur di Jepang mengalami peningkatan yang cukup drastis dalam hal produktifitas. Sehingga konsep tersebut disalin secara luas di seluruh dunia.
+        </p>
+        <p>
+          Kaizen juga berlaku untuk individu.
+        </p>
+        <p>
+          Setiap hari, niatkan diri kita bekerja untuk menambah skill yang kita miliki secara terus menerus, meskipun dalam skala yang kecil. Berbeda dengan rumput di Kebun Raya Bogor yang perlu waktu puluhan tahun, kita akan segera melihat hasilnya dalam hitungan hari. Selama bertahun-tahun, kita akan kagum bagaimana pengalaman kita telah berkembang dan skill kita juga jauh meningkat dari sebelumnya.
+        </p>
+      </article>
+      <rg-label label="engineering"/>
+      <rg-label label="pragmatic programmer"/>
+      <rg-label label="bukalapak"/>
+      <rg-label label="software engineering"/>
+      <rg-label label="programmer"/>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Apa yang membuat programmer pragmatis? | Rizal Asrul Pambudi',
+    meta: [
+      {
+        name: 'keywords',
+        content: 'engineering, pragmatic programmer, bukalapak, software engineering, programmer, rizal, asrul, pambudi',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Sebelum membahas mengenai pragmatic programmer, kita harus mengerti apa yang dimaksud dengan pragmatic. Siapapun bisa googling untuk mencari tahu apa yang dimaksud dengan kata “pragmatic”. Tapi saya mencoba untuk mendemostrasikan apa yang dimaksud dengan pragmatic yang sebenarnya.'
+      },
+      { property: 'og:url', content: 'https://rizalasrul.netlify.app/apa-yang-membuat-programmer-pragmatis' },
+      {
+        property: 'og:site_name',
+        content: 'Apa yang membuat programmer pragmatis? | Rizal Asrul Pambudi',
+      },
+      {
+        property: 'og:title',
+        content: `Apa yang membuat programmer pragmatis? | Rizal Asrul Pambudi`,
+      },
+      {
+        property: 'og:description',
+        content: 'Sebelum membahas mengenai pragmatic programmer, kita harus mengerti apa yang dimaksud dengan pragmatic. Siapapun bisa googling untuk mencari tahu apa yang dimaksud dengan kata “pragmatic”. Tapi saya mencoba untuk mendemostrasikan apa yang dimaksud dengan pragmatic yang sebenarnya.',
+      },
+      {
+        property: 'og:image',
+        content: 'https://rizalasrul.netlify.app/images/blog_thumbnails/apa-yang-membuat-programmer-pragmatis.webp',
+      },
+    ],
+    link: [{ rel: 'canonical', href: 'https://rizalasrul.netlify.app/apa-yang-membuat-programmer-pragmatis' }],
+  },
+};
+</script>
