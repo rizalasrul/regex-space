93b2b32fe6404fdb04f18cdd5becde2600cf68d1
Rizal Asrul Pambudi <rizal.asrul@live.com>
2020-09-14 15:46:55 +0700

    feat: Add new article /berkomunikasi-efektif-sebagai-software-engineer

---
+++ pages/berkomunikasi-efektif-sebagai-software-engineer.vue
@@ -0,0 +1,164 @@
+<template>
+  <section>
+    <div class="mx-auto pt-24 px-12 py-24">
+      <center>
+        <div>
+          <h1 class="font-semibold text-3xl text-gray-700">Berkomunikasi efektif sebagai software engineer</h1>
+          <hr class="w-2/5" style="
+            margin-top: 10px;
+            margin-bottom: 10px;
+            border: 1px solid #c3c3c3;
+            ">
+          <p class="text-gray-600 text-lg">10 Februari 2020</p>
+        </div>
+      </center>
+    </div>
+
+    <div class="container mx-auto pb-24 px-12">
+      <article class="pb-12 prose prose-xl" style="max-width: none;">
+        <figure>
+          <img src="~/static/images/blog_thumbnails/berkomunikasi-efektif-sebagai-software-engineer.webp" alt="Photo from Unplash">
+        </figure>
+        <blockquote>
+          <p>I believe that it is better to be looked over than it is to be overlooked (Mae West, Belle of the Nineties, 1934)</p>
+        </blockquote>
+        <p>
+          Kita bisa belajar dari kutipan di atas. Bukan hanya apa yang kita pikirkan, tapi bagaimana cara kita mengemasnya. Memiliki ide-ide terbaik, <i>code</i> dengan pendekatan terbaik, atau pemikiran paling pragmatis sekalipun pada akhirnya akan menguap, kecuali kita dapat menyampaikannya kepada orang lain. Ide yang cemerlang akan sia-sia jika tanpa komunikasi yang efektif.
+        </p>
+        <p>
+          Sebagai developer, kita juga berkomunikasi di berbagai jenjang. Kita menghabiskan berjam-jam dalam rapat, mendengarkan, dan berbicara. Kita juga bertemu dengan <i>end user</i>, mencoba memahami apa yang mereka butuhkan. Kita juga menulis kode, secara tidak langsung kita juga berkomunikasi dengan mesin dan menuangkan pemikiran kita ke dalam baris kode. Kita juga menulis SAD (<i>service architecture diagram</i>) atau memo untuk melaporkan status pekerjaan, atau mungkin memberikan saran kepada atasan jika kita menemukan pendekatan yang baru. Dan tidak lupa, kita juga bekerja dalam tim. Sebagian besar hari kita habiskan untuk berkomunikasi di berbagai jenjang dan dengan berbagai bentuk. Jadi, kita perlu melakukannya dengan baik.
+        </p>
+        <p>
+          Perlakukan “bahasa indonesia” (atau apapun bahasa yang kita pakai) selayaknya bahasa pemrograman lain. Tulis percakapan kita sebagai mana kita menulis kode: hormati prinsip DRY (<i>don’t repeat yourself</i>), ETC (<i>easier to change</i>).
+        </p>
+        <blockquote>
+          <p>English is just another programming language</p>
+        </blockquote>
+        <h2>Know your audience</h2>
+        <p>
+          Kita berkomunikasi hanya jika kita ada informasi yang perlu disampaikan. Untuk melakukan itu, kita perlu memahami kebutuhan, minat, dan kemampuan lawan bicara kita. Bayangkan kita sedang rapat dimana tim product engineer sedang menatap dengan tatapan tajam ke arah VP of Marketing dengan monolog panjang mengenai manfaat dari penggunaan microservice. Situasi tersebut bukan komunikasi. Itu hanya ceramah, dan itu sangat membosankan.
+        </p>
+        <p>
+          Katakanlah kita ingin menyarankan penggunaan microservice tersebut untuk <i>campaign</i> harbolnas tahun depan. Kita dapat memaparkan sistem dari microservice tersebut dalam beberapa bentuk yang berbeda, tergantung dari audiens yang kita ajak bicara. Paparkan bahwa dengan memakai pendekatan tersebut, pelanggan akan lebih mudah menemukan banner campaign, cukup dari homepage aplikasi atau website kita. Tim marketing juga akan lebih mudah mengunggah banner campaign sesuai dengan target market yang diincar. Para manager di divisi marketing setidaknya memiliki dua alasan positif untuk menggunakan microservice tersebut: tim marketing dapat dengan mudah menjalankan campaign mereka dan target market yang tepat sasaran. Akhirnya, ide yang tim product engineer usulkan dapat terlihat manfaatnya bahkan oleh orang non-tech sekalipun. Dan peserta rapat juga akan bersemangat untuk mengeksekusi proyek tersebut.
+        </p>
+        <h2>Know what you want to say</h2>
+        <p>
+          Mungkin bagian tersulit dari gaya komunikasi yang lebih formal adalah mengetahu apa yang ingin kita tulis. Para penulis novel fiksi sering merencanakan buku-buku mereka secara terperinci sebelum mereka mulai menulis. Tetapi orang-orang yang menulis dokumen teknis sering duduk di depan keyboard, dan menulis:
+        </p>
+        <ol>
+          <li>Pendahuluan</li>
+        </ol>
+        <p>
+          Rencanakan apa yang ingin kita katakan. Tulis garis besarnya. Lalu tanyakan pada diri kita sendiri, “<i>does this communicate what I want to express to my audience in a way that works for them?</i>”. Sempurnakan tulisan tersebut hingga menurut kita sudah jelas.
+        </p>
+        <p>
+          Pendekatan ini tidak hanya berlaku untuk penulisan dokumen. Saat kita dihadapkan dengan rapat penting atau panggilan telepon, catat ide-ide yang ingin kita komunikasikan, dan rencanakan beberapa strategi untuk menyampaikannya.
+        </p>
+        <p>
+          Sekarang setelah kita tahu apa yang diinginkan audiens kita, sekarang saatnya merencanakan bagaimana cara melakukannya.
+        </p>
+        <h2>Choose your moment</h2>
+        <p>
+          Bayangkan sekarang pukul enam sore di hari Jumat. Beberapa dari rekan kerja kita sudah berkemas-kemas untuk meninggalkan meja kerja mereka. Ada yang langsung pulang ke rumah, ada juga yang mampir ke kafe untuk bertemu kawan lama mereka. Mereka siap untuk menyambut weekend. Dan ini mungkin bukan saat yang tepat untuk kita meminta mereka melakukan <i>code review</i> untuk PR kita.
+        </p>
+        <p>
+          Sebagai bagian dari “know your audience”, kita perlu tahu prioritas mereka saat itu. Sekarang hari jumat dan rekan kerja kita ingin mengakhiri jam kerja mereka. Code review tentulah bukan suatu yang mendesak. Kita bisa menunda hingga hari senin untuk meminta bantuannya. Terkadang yang kita perlukan hanyalah pertanyaan sederhana, “apakah ini saat yang tepat untuk membicarakannya?”.
+        </p>
+        <h2>Choose a style</h2>
+        <p>
+          Sesuaikan gaya tulisan dengan orang yang akan membaca tulisan kita. Beberapa orang menginginkan tulisan yang <i>to-the-point</i>, beberapa yang lain menyukai obrolan panjang sebelum menuju ke inti pesan. Perhatikan juga tingkat keahlian dan pengalaman mereka. Apakah mereka sudah ahli, atau masih pemula?
+        </p>
+        <p>
+          Jika kita melihat orang yang menerima pesan kita merasa kesulitan memahami gaya bahasa kita, maka selalu terbukalah terhadap <i>feedback</i>. Dari masukan mereka, kita bisa menjadi lebih baik.
+        </p>
+        <h2>Make it look good</h2>
+        <blockquote>
+          <p>Your ideas are important. They deserve a good-looking vehicle to convey them to your audience.</p>
+        </blockquote>
+        <p>
+          Banyak software engineer (mungkin di tingkat manager) fokus hanya pada konten saat membuat dokumentasi tertulis. Akan lebih baik jika kita juga memperhatikan bagaimana cara menyampaikan pesan, diksi yang kita pilih, hingga susunan paragraf yang membuat pembaca menjadi nyaman.
+        </p>
+        <figure class="px-64">
+          <img src="~/static/images/blog_contains/berkomunikasi-efektif-sebagai-software-engineer-1.webp" alt="Menanam kangkung dengan metode wick">
+          <figcaption>Photo by <a href="https://unsplash.com/@muay0051" target="_blank">Siriwan Arunsiriwattana</a> on <a href="https://unsplash.com/photos/gs0coXLmjdI" target="_blank">Unsplash</a></figcaption>
+        </figure>
+        <p>
+          Saat ini tidak ada alasan untuk tidak menghasilkan dokumentasi yang buruk. Banyak perangkat lunak modern yang dapat menghasilkan output yang baik, terlepas apakah kita sedang menulis menggunakan markdown, atau menggunakan <i>word processor</i>. Kita hanya perlu mempelajari beberapa perintah dasar. Jika kita menggunakan word processor (Word misalnya), gunakan style sheet nya supaya konsisten. Pelajari cara mengatur header dan footer halaman. Lihat pula sampel dokumen (biasanya perusahaan sudah menyediakan ini sebagai acuan). Periksa ejaan kata supaya tidak ada yang <i>miss</i> di pembaca.
+        </p>
+        <h2>Involve your audience</h2>
+        <p>
+          Kita sering menemukan bahwa dokumen yang kita hasilkan berakhir kurang penting dibandingkan dengan proses yang kita lalui untuk menghasilkanya. Jika memungkinkan, libatkan target pembaca dokumen kita dari awal. Dapatkan feedback dari mereka. Kita bisa membangun hubungan kerja yang baik dari proses tersebut.
+        </p>
+        <h2>Be a listener</h2>
+        <p>
+          Hanya ada satu teknik yang harus kita gunakan supaya orang mau mendengarkan kita: <b>dengarkan mereka terlebih dahulu</b>. Ketika kita menjadi salah satu orang yang paling paham tentang sebuah konsep di sebuah forum, atau ketika kita sedang presentasi di depan puluhan orang berdasi, jika kita tidak mendengarkan mereka, merekapun juga tidak akan mendengarkan kita.
+        </p>
+        <p>
+          Dorong mereka untuk berbicara dengan cara menanyakan pertanyaan, atau juga bisa dengan cara meminta mereka untuk merangkum apa yang sudah kita sampaikan ke mereka. Ubah rapat yang kaku menjadi dialog.
+        </p>
+        <h2>Get back to people</h2>
+        <p>
+          Jika kita bertanya ke seseorang secara langsung, mereka akan merasa tidak sopan jika tidak menjawab pertanyaan kita. Tapi seberapa sering kita gagal mendapatkan respon dari orang lain ketika kita bertanya tentang sebuah informasi melalui email atau memo? Dalam kesibukan sehari-hari, hal tersebut sering terlupakan. Jika kita mendapatkan email dari orang lain, selalu tanggapi dengan segera, meskipun hanya dengan “saya akan menghubungimu nanti”. Tindakan seperti itu membuat mereka merasa bahwa kita tidak melupakan mereka.
+        </p>
+        <p>
+          Kuncinya adalah efektif. Sebisa mungkin jadikan komunikasi yang kita jalin seefektif mungkin, meskipun kegiatan komunikasi tersebut tidak terjadi secara langsung <i>face-to-face</i>.
+        </p>
+        <h2>Documentation</h2>
+        <p>
+          Biasanya, programmer atau software engineer tidak terlalu memperhatikan dokumentasi. Dokumentasi sering dianggap sebagai kebutuhan yang tidak menguntungkan, atau setidaknya jika kita mengerjakan dokumentasi, tugas tersebut diberikan prioritas yang paling rendah. “Iyalah. Lebih penting bikin fitur A daripada harus mengorbankan waktu untuk bikin dokumentasi”.
+        </p>
+        <p>
+          Seharusnya dokumentasi bisa dijadikan sebagai bagian integral dari seluruh proses development. Dengan adanya dokumentasi, pekerjaan kita akan menjadi lebih mudah karena kita tidak perlu membuang waktu ataupun “copy-paste” pekerjaan kita sebelumnya.
+        </p>
+        <p>
+          Sangat mudah untuk menghasilkan dokumentasi yang terlihat bagus dari comment yang kita selipkan di <i>source code</i>, dan disarankan juga untuk menyelipkan comment ke modul atau fungsi yang di-<i>export</i> supaya programmer lain dapat memahami dengan cepat.
+        </p>
+        <p>
+          Akan tetapi, bukan berarti menambahkan comment di setiap fungsi, <i>data structure</i>, deklarasi, dll itu dibenarkan. Cara seperti ini hanya cocok untuk <i>short-term solution</i>. Jika kita selalu menambahkan comment setiap kita menambahkan fungsi, berarti fungsi yang kita buat tidak <i>explicit</i> karena perlu adanya penjelasan dari luar alih-alih fungsi yang ditulis sudah menjelaskan logika yang dikerjakan.
+        </p>
+      </article>
+      <rg-label label="communication"/>
+      <rg-label label="documentation"/>
+      <rg-label label="software engineer"/>
+      <rg-label label="effective"/>
+      <rg-label label="ideas"/>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Berkomunikasi efektif sebagai software engineer | Rizal Asrul Pambudi',
+    meta: [
+      {
+        name: 'keywords',
+        content: 'communication, documentation, software engineer, effective, ideas, rizal, asrul, pambudi',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Kita bisa belajar dari kutipan di atas. Bukan hanya apa yang kita pikirkan, tapi bagaimana cara kita mengemasnya. Memiliki ide-ide terbaik, code dengan pendekatan terbaik, atau pemikiran paling pragmatis sekalipun pada akhirnya akan menguap, kecuali kita dapat menyampaikannya kepada orang lain. Ide yang cemerlang akan sia-sia jika tanpa komunikasi yang efektif.'
+      },
+      { property: 'og:url', content: 'https://rizalasrul.netlify.app/berkomunikasi-efektif-sebagai-software-engineer' },
+      {
+        property: 'og:site_name',
+        content: 'Berkomunikasi efektif sebagai software engineer | Rizal Asrul Pambudi',
+      },
+      {
+        property: 'og:title',
+        content: `Berkomunikasi efektif sebagai software engineer | Rizal Asrul Pambudi`,
+      },
+      {
+        property: 'og:description',
+        content: 'Kita bisa belajar dari kutipan di atas. Bukan hanya apa yang kita pikirkan, tapi bagaimana cara kita mengemasnya. Memiliki ide-ide terbaik, code dengan pendekatan terbaik, atau pemikiran paling pragmatis sekalipun pada akhirnya akan menguap, kecuali kita dapat menyampaikannya kepada orang lain. Ide yang cemerlang akan sia-sia jika tanpa komunikasi yang efektif.',
+      },
+      {
+        property: 'og:image',
+        content: 'https://rizalasrul.netlify.app/images/blog_thumbnails/berkomunikasi-efektif-sebagai-software-engineer.webp',
+      },
+    ],
+    link: [{ rel: 'canonical', href: 'https://rizalasrul.netlify.app/berkomunikasi-efektif-sebagai-software-engineer' }],
+  },
+};
+</script>
