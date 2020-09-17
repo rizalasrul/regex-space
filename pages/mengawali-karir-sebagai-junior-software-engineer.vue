a4606384cec181adcbd751d2d4d81cfd9de57962
Rizal Asrul Pambudi <rizal.asrul@live.com>
2020-09-17 11:03:58 +0700

    feat: New article /mengawali-karir-sebagai-junior-software-engineer

---
+++ pages/mengawali-karir-sebagai-junior-software-engineer.vue
@@ -0,0 +1,141 @@
+<template>
+  <section>
+    <rg-heading-content
+      title="Mengawali karir sebagai junior software engineer"
+      sub-title="27 Januari 2020" />
+
+    <div class="container mx-auto xl:pb-24 pb-12 xl:px-64 md:px-24 px-4">
+      <article class="xl:pb-12 pb-6 prose xl:prose-xl sm:prose-sm md:prose-lg" style="max-width: none;">
+        <figure>
+          <img src="~/static/images/blog_thumbnails/mengawali-karir-sebagai-junior-software-engineer.webp" alt="Photo from Unplash">
+          <figcaption>Photo by <a href="https://unsplash.com/@napr0tiv" target="_blank">Vasily Koloda</a> on <a href="https://unsplash.com/photos/8CqDvPuo_kI" target="_blank">Unsplash</a></figcaption>
+        </figure>
+        <p>
+          Kamu adalah freshgradute yang baru saja lulus wisuda beberapa minggu lalu. Dua hari setelahnya, kamu mulai bergabung dengan perusahaan yang kamu idam-idamkan selama ini.
+        </p>
+        <p>
+          Hari yang ditunggu-tunggu pun tiba. Hari dimana kamu melangkah ke pintu depan kantormu. <i>Yup, your first day of work as a professional software engineer</i>. Kamu merasa sangat bersemangat dalam melaksanakan task apapun yang diembankan ke dirimu. Kamu merasa siap. Empat tahun mengenyam bangku kuliah pasti memberikan bekal yang sangat cukup untuk menghadapi dunia kerja.
+        </p>
+        <p>
+          Setelah beberapa saat, percikan motivasi perlahan mulai memudar ketika kamu mengetahui bahwa banyak hal yang belum kamu kuasai. Tech stack yang kamu pelajari di kuliah jauh berbeda dengan apa yang benar-benar digunakan di perusahaan tersebut. Kamu mengajukan banyak pertanyaan sepele sehingga kamu mulai merasa bahwa seniormu menatapmu seolah-olah dia bertanya <i>“bagaimana mungkin orang yang tidak kompeten ini diterima di sini?”</i>.
+        </p>
+        <p>
+          Sebelum berpikir lebih jauh, mulai tarik nafas dalam-dalam, dan yakinkan dirimu bahwa itu semua hanya terjadi di dalam kepalamu. Tiap orang pasti pernah berada di posisimu, sebagai junior software engineer yang berstatus freshgraduate. Menjadi junior adalah kesempatan yang sangat baik untuk belajar banyak hal.
+        </p>
+        <h2>Engineering best practice</h2>
+        <figure>
+          <img src="~/static/images/blog_contains/mengawali-karir-sebagai-junior-software-engineer-1.webp" alt="Photo from Unplash">
+          <figcaption>Photo by <a href="https://www.360logica.com/blog/the-importance-of-daily-stand-up-meetings-in-scrum/" target="_blank">360logica</a></figcaption>
+        </figure>
+        <p>
+          Setiap perusahaan pasti memiliki best practice nya masing-masing. Hal pertama yang wajib kita sesuaikan adalah <i>engineering culture</i> yang ada di perusahaan tempat kita bekerja. Pertanyaan-pertanyaan seperti “apakah menulis unit test adalah sebuah kewajiban?”, “bagaimana konvensi penulisan kode yang diterapkan?”, hingga “jika terjadi emergency, langkah-langkah apa yang harus dilakukan” harus sudah jelas.
+        </p>
+        <p>
+          Kita juga diharapkan untuk memahami dan berpartisipasi dalam praktik alur kerja perusahaan secara aktif dan tepat waktu. Jika perusahaan tersebut menerapkam metode agile dengan framework SCRUM, maka kita akan terbiasa dengan:
+        </p>
+        <ul>
+          <li>Daily standup (termasuk jika kita sedang remote)</li>
+          <li>Marketplace</li>
+          <li>Sprint planning</li>
+          <li>Retrospective</li>
+        </ul>
+        <h2>Communication</h2>
+        <p>
+          Meskipun junior, kita selalu diperbolehkan untuk speak up ketika dihadapkan dengan sesuatu yang menghambat pekerjaan kita. Sebagai contoh, kita bisa menghubungi kembali reviewer MR kita untuk segera mereview, daripada hanya diam saja menunggu reviewer mengecek kode kita.
+        </p>
+        <p>
+          Ketika sedang meeting, jangan melakukan asumsi ketika requirement tidak jelas. Mintalah klarifikasi supaya apa yang kita kerjakan sesuai dengan ekspektasi yang diharapkan. Berasumsi menjadikan tim kita tidak efektif dan buruk dalam berkolaborasi.
+        </p>
+        <p>
+          Bekerja di tim yang besar dengan berbagai macam divisi dan fungsi, akan selalu ada obstacle. Jika kita kebetulan melihat obstacle tersebut, eskalasi kan hal itu. Bisa dengan cara eskalasi ke peer, team lead, atau bahkan manager kita. Perhatikan juga bagaimana cara kita menyampaikan ide, karena cara kita menyampaikan gagasan itu penting.
+        </p>
+        <p>
+          Meskipun kita seorang junior, kita juga memiliki ilmu yang berharga untuk tim. Kita juga memiliki suara yang sama dengan engineer lain, dan tim berharap kita dapat berintegrasi dengan baik dan dapat menunjukkan sisi proaktif untuk membantu rekan setim kita tanpa diminta.
+        </p>
+        <h2>Own your product</h2>
+        <p>
+          Pahami produk atau fitur yang ada di tim kita dan seberapa besar dampak yang diterima oleh pelanggan. Kita juga bisa aktif menanggapi jika terjadi production issue. Dan ketika production issue tersebut berdampak ke tim, kita juga bisa membuat postmortem termasuk mengidentifikasi akar permasalahannya dan langkah-langkah pencegahannya.
+        </p>
+        <p>
+          Selain itu kita juga perlu memahami tech stack yang digunakan serta product metrics di tim kita.
+        </p>
+        <h2>Pragmatism</h2>
+        <p>
+          Menjadi junior berarti akan banyak task technical yang diberikan ke kita. Tujuannya adalah untuk memberikan pemahaman kepada kita bagaimana technical flow yang ada di perusahaan itu. Kita harus konsisten untuk mengerjakan task sesuai dengan prioritas yang sudah diberikan.
+        </p>
+        <p>
+          Gunakan waktu kita ketika junior untuk banyak belajar. Kerjakan task tersebut secepat mungkin dengan tetap menjaga kualitas kode yang kita tulis. Semakin cepat task yang kita selesaikan, maka akan semakin banyak task lain yang kita kerjakan. Bisa jadi task tersebut cukup bervariasi dan menjadikan ladang kita untuk banyak eksplorasi teknologi.
+        </p>
+        <p>
+          Sebagai junior, kita perlu sering melakukan code review dan mentoring untuk dapat menangkap bug sejak dini. Code review juga membantu kita dalam melalukan refactor sehingga kode kita menjadi lebih maintainable.
+        </p>
+        <p>
+          Jangan lupa untuk selalu menerapkan best practice dan sebisa mungkin menghindari adanya technical debt.
+        </p>
+        <h2>Strive for mastery</h2>
+        <p>
+          Kesalahan adalah suatu keniscayaan. Dan ketika itu terjadi, maka akan selalu ada ruang untuk belajar. Kita juga harus bersedia menerima feedback tanpa bersikap defensif.
+        </p>
+        <blockquote>
+          <p>Everybody makes mistakes. The difference is based on how they respond to the mistake.</p>
+        </blockquote>
+        <p>
+          Pelajari arsitektur teknologi atau service yang diterapkan di tim kita. Dan juga secara aktif memberi masukan dan menerapkannya terhadap tim kita.
+        </p>
+        <h2>Delivering results</h2>
+        <figure>
+          <img src="~/static/images/blog_contains/mengawali-karir-sebagai-junior-software-engineer-2.webp" alt="Photo from Unplash">
+          <figcaption>Pair programming adalah cara yang efektif untuk saling tukar wawasan</figcaption>
+        </figure>
+        <p>
+          Kita diharapkan dapat memberikan dampak yang baik oleh perusahaan. Maka kita diharapkan dapat menerapkan solusi dalam permasalahan yanf terdefinisi dengan baik.
+        </p>
+        <p>
+          Kita sebaiknya memiliki peer yang lebih senior supaya kita dapat diarahkan dengan baik. Bila perlu mintalah untuk melakukan praktik pair programming supaya transfer knowledge yang terjadi dapat efisien.
+        </p>
+      </article>
+      <rg-label label="junior"/>
+      <rg-label label="programming"/>
+      <rg-label label="software engineer"/>
+      <rg-label label="career"/>
+      <rg-label label="scrum"/>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Mengawali karir sebagai junior software engineer | Rizal Asrul Pambudi',
+    meta: [
+      {
+        name: 'keywords',
+        content: 'junior, programming, software engineer, career, scrum, rizal, asrul, pambudi',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Pahami bahwa memulai karir sebagai junior software engineer berarti tanggung jawab utama kita adalah untuk belajar dan menyerap sebanyak-banyaknya best practice di tempat kita bekerja. Harap mengerti bahwa sebagai junior, kita akan sering melakukan kesalahan dan sesekali membuat bug, dan itu tidak apa-apa. Itu adalah bagian dari proses pembelajaran yang harus kita lalui.'
+      },
+      { property: 'og:url', content: 'https://rizalasrul.netlify.app/mengawali-karir-sebagai-junior-software-engineer' },
+      {
+        property: 'og:site_name',
+        content: 'Mengawali karir sebagai junior software engineer | Rizal Asrul Pambudi',
+      },
+      {
+        property: 'og:title',
+        content: `Mengawali karir sebagai junior software engineer | Rizal Asrul Pambudi`,
+      },
+      {
+        property: 'og:description',
+        content: 'Pahami bahwa memulai karir sebagai junior software engineer berarti tanggung jawab utama kita adalah untuk belajar dan menyerap sebanyak-banyaknya best practice di tempat kita bekerja. Harap mengerti bahwa sebagai junior, kita akan sering melakukan kesalahan dan sesekali membuat bug, dan itu tidak apa-apa. Itu adalah bagian dari proses pembelajaran yang harus kita lalui.',
+      },
+      {
+        property: 'og:image',
+        content: 'https://rizalasrul.netlify.app/images/blog_thumbnails/mengawali-karir-sebagai-junior-software-engineer.webp',
+      },
+    ],
+    link: [{ rel: 'canonical', href: 'https://rizalasrul.netlify.app/mengawali-karir-sebagai-junior-software-engineer' }],
+  },
+};
+</script>
