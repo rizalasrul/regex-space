12c05bf872a5e0ebf226a2b4c98b30f00f48a366
Rizal Asrul Pambudi <rizal.asrul@live.com>
2020-09-17 14:45:33 +0700

    feat: Add new article /tentang-bekerja-di-tech-startup

---
+++ pages/tentang-bekerja-di-tech-startup.vue
@@ -0,0 +1,118 @@
+<template>
+  <section>
+    <rg-heading-content
+      title="Tentang kerja di startup teknologi"
+      sub-title="06 Januari 2020" />
+
+    <div class="container mx-auto xl:pb-24 pb-12 xl:px-64 md:px-24 px-4">
+      <article
+        class="xl:pb-12 pb-6 prose xl:prose-xl sm:prose-sm md:prose-lg"
+        style="max-width: none;">
+        <figure>
+          <img src="~/static/images/blog_thumbnails/tentang-bekerja-di-tech-startup.webp" alt="Photo by Dawn Armfield on Unsplash">
+        </figure>
+        <p>
+          Kalau kita bicara tentang bagaimana rasanya kerja di tech startup, kita perlu mengerti apa arti <i>startup</i> itu sendiri. Ini adalah salah satu definisi yang menurut saya paling pas dari presiden dan co-founder Bukalapak tentang arti dari startup.
+        </p>
+        <blockquote>
+          <p>Startup adalah bisnis yang berusaha untuk menyelesaikan suatu masalah dengan solusi yang belum teruji, belum scalable, atau belum terbukti sustainable secara finansial. (Muhamad Fajrin Rasyid, Ex-President & Co-founder Bukalapak)</p>
+        </blockquote>
+        <p>
+          Dari sini kita bisa ambil kata kunci “belum teruji”, “belum scalable”, dan “belum terbukti sustainable secara finansial”. Artinya, apa yang dibangun oleh startup belum tentu bisa berkelanjutan. Startup mencoba membangun sebuah produk atau solusi baru yang sebelumnya tidak ada, sambil melihat apakah solusi itu memang <i>valuable</i> atau tidak di masyarakat.
+        </p>
+        <p>
+          Semisal ada orang bikin usaha supermarket atau rumah makan, usaha tersebut bukan termasuk startup. Karena usaha-usaha itu adalah solusi yang sudah terbukti ada yang berhasil dan model bisnisnya sudah ada sebelumnya.
+        </p>
+        <p>
+          Sehingga kunci dari startup adalah <b>inovasi</b>. Startup akan melakukan sesuatu yang belum teruji apakah solusi yang ditawarkan benar-benar bekerja di masyarakat. Sehingga startup harus bisa menggabungkan kreatifitas dan logika untuk menghasilkan sebuah solusi yang belum ada sebelumnya.
+        </p>
+        <h2>Kualitas tetap yang utama</h2>
+        <p>
+          Karena startup akan selalu berinovasi, sehingga tech startup memerlukan talenta yang memiliki kualitas baik. Jika ingin berkecimpung di dunia startup, sudah pasti kita harus mengasah skill yang kita miliki dan selalu mempelajari tren teknologi yang terbaru. Mengikuti <i>online course</i> atau saling berbagi pengetahuan dengan rekan kerja merupakan beberapa contoh hal yang bisa dilakukan supaya kita bisa selalu berinovasi.
+        </p>
+        <p>
+          Memperbarui skill ini penting sekali karena teknologi akan selalu berkembang. Misalnya kita bekerja di sebuah startup pada awal tahun 2009, yang mana <i>smartphone</i> pada saat itu belum menjamur seperti sekarang. Sehingga ketika kita mengembangkan sebuah website, kita cukup fokus pada platform desktop saja. Berselang sebelas tahun kemudian, mayoritas pengunjung website adalah pengguna smartphone, sehingga banyak perusahaan, termasuk tech startup yang berlomba-lomba menjadikan dirinya sebagai <i>mobile first company</i>. Maka mengembangkan website di platform mobile juga menjadi hal yang penting, dan sebagai talenta yang bekerja di startup tersebut, harus siap dengan tren teknologi yang akan selalu berkembang.
+        </p>
+        <h2>Adaptasi dengan budaya startup</h2>
+        <p>
+          Setelah memastikan <i>hardskill</i> kita terpenuhi, maka selanjutnya adalah memastikan bahwa kita dapat memahami budaya tempat kita bekerja dan memahami apa yang diharapkan oleh startup dari karyawan mereka. Tentu saja budaya setiap startup itu beragam. Tetapi sepengamatan saya, budaya yang pasti ada adalah:
+        </p>
+        <h3>1. Budaya untuk selalu bereksperimen dan berinovasi</h3>
+        <p>
+          Seperti yang sudah saya tulis di atas, bahwa startup itu sudah pasti akan selalu berinovasi. Selain itu yang tidak kalah penting adalah budaya bereksperimen. Biasanya startup memberikan kelonggaran atau fleksibilitas terhadap karyawannya untuk melakukan eksperimen-eksperimen terkait bidang pekerjaan mereka. Kita cukup mendapatkan arahan <i>high level</i> tanpa harus diberikan list mulai A ke Z dari apa yang harus dikerjakan.
+        </p>
+        <p>
+          Hal ini berarti kita dituntut untuk dewasa dalam bertindak. Meskipun tidak terjadi <i>micromanage</i>, sangat penting untuk kita tetap berpegang terhadap KPI (Key Performance Indicator) atau tanggung jawab kita, dan bersikap profesional terhadap hal itu.
+        </p>
+        <p>
+          Di Bukalapak sendiri memiliki core value <b>try, fail, and try again</b>, sehingga Bukalapak berharap talentanya berani untuk bereksperimen dan juga berani menerima ketika menemui kegagalan, serta belajar dari kegagalan tersebut. Core value yang lain adalah <b>go the extra mile</b> juga berlaku. Bukalapak juga berharap talentanya tidak hanya berhenti ketika KPI mereka tercapai. Tetapi terus berinovasi untuk mencapai tanggung jawab yang lebih baik lagi kedepannya.
+        </p>
+        <h3>2. Budaya untuk bekerja cepat</h3>
+        <p>
+          Secara umum, semua startup memiliki budaya kerja yang cepat jika dibandingkan dengan <i>corporate company</i>. Alur kerja di tech startup pada umumnya menerapkan metode agile atau semacamnya untuk menghasilkan outcome dengan cepat.
+        </p>
+        <p>
+          Pengalaman pribadi saya, apa yang saya pelajari di Bukalapak (saat itu saya baru tiga bulan bergabung) jauh lebih banyak daripada apa yang saya pelajari di tempat sebelumnya, padahal di tempat sebelumnya, saya bekerja selama sekitar setahun.
+        </p>
+        <p>
+          Lagi-lagi disinilah penting bagi kita untuk selalu mengasah diri. Mungkin saja tanggung jawab baru yang kita emban adalah bidang yang tidak sesuai atau tidak kita kuasai. Atau mungkin saja teknologi yang kita pelajari beberapa tahun sebelumnya sudah tidak relevan lagi untuk beberapa bulan kedepan.
+        </p>
+        <h3>3. Nyaman ketika bekerja dalam tim</h3>
+        <figure>
+          <img
+            src="~/static/images/blog_contains/tentang-bekerja-di-tech-startup-1.webp"
+            alt="Photo by Mimi Thian on Unsplash">
+          <figcaption>Photo by <a href="https://unsplash.com/@mimithian" target="_blank">Mimi Thian</a> on <a href="https://unsplash.com/photos/vdXMSiX-n6M" target="_blank">Unsplash</a></figcaption>
+        </figure>
+        <p>
+          Pada akhirnya, bekerja dalam startup adalah bekerja dalam tim. Biasanya, keryawan startup bekerja dalam tim yang terdiri dari berbagai keahlian dan bidang yang berbeda. Oleh karena itu penting bagi kita untuk berempati satu sama lain dan menempatkan tujuan tim sebagai prioritas yang utama.
+        </p>
+        <p>
+          Sepengamatan saya, satu tim product engineer memiliki anggota dengan role yang berbeda-beda. Secara umum terdiri dari backend engineer, frontend engineer, mobile apps developer (android dan ios), test engineer, data scientist, dan dipimpin oleh engineering manager, test engineering manager, data manager, serta product manager.
+        </p>
+      </article>
+      <rg-label label="startup"/>
+      <rg-label label="culture"/>
+      <rg-label label="software engineering"/>
+      <rg-label label="bukalapak"/>
+      <rg-label label="product engineering"/>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Tentang kerja di startup teknologi | Rizal Asrul Pambudi',
+    meta: [
+      {
+        name: 'keywords',
+        content: 'emergency, deployment, software engineering, testing, solving, rizal, asrul, pambudi',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Talenta seperti apa sih yang dicari oleh tech startup?'
+      },
+      { property: 'og:url', content: 'https://rizalasrul.netlify.app/tentang-bekerja-di-tech-startup' },
+      {
+        property: 'og:site_name',
+        content: 'Tentang kerja di startup teknologi | Rizal Asrul Pambudi',
+      },
+      {
+        property: 'og:title',
+        content: `Tentang kerja di startup teknologi | Rizal Asrul Pambudi`,
+      },
+      {
+        property: 'og:description',
+        content: '',
+      },
+      {
+        property: 'og:image',
+        content: 'https://rizalasrul.netlify.app/images/blog_thumbnails/tentang-bekerja-di-tech-startup.webp',
+      },
+    ],
+    link: [{ rel: 'canonical', href: 'https://rizalasrul.netlify.app/tentang-bekerja-di-tech-startup' }],
+  },
+};
+</script>
