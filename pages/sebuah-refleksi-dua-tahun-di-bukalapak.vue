991cdf52b187c26a76c4343e13f2a26ca09e47f0
Rizal Asrul Pambudi <rizal.asrul@live.com>
2020-09-17 11:38:30 +0700

    feat: Add new article /sebuah-refleksi-dua-tahun-di-bukalapak

---
+++ pages/sebuah-refleksi-dua-tahun-di-bukalapak.vue
@@ -0,0 +1,163 @@
+<template>
+  <section>
+    <rg-heading-content
+      title="Sebuah refleksi dua tahun di Bukalapak"
+      sub-title="20 Januari 2020" />
+
+    <div class="container mx-auto xl:pb-24 pb-12 xl:px-64 md:px-24 px-4">
+      <article class="xl:pb-12 pb-6 prose xl:prose-xl sm:prose-sm md:prose-lg" style="max-width: none;">
+        <figure>
+          <img src="~/static/images/blog_thumbnails/sebuah-refleksi-dua-tahun-di-bukalapak.webp" alt="Photo from Unplash">
+          <figcaption>Onboarding pertama</figcaption>
+        </figure>
+        <p>
+          Sebelum di Bukalapak, saya kerja di salah satu FCMG company sebagai IT developer selama kurang lebih setahun. Selama di sana, saya menyadari bahwa saya ingin menjadi lebih bebas bereksperimen atau mandiri dalam membangun proyek-proyek yang memiliki dampak lebih besar.
+        </p>
+        <p>
+          Momen itu dimulai saat teman saya mengajak untuk bergabung dengan Bukalapak. Pada saat itu dia sudah bergabung sebagai mobile apps developer. Dia bercerita bagaimana hal kecil yang dia kerjakan dapat memberi dampak bagi jutaan pengguna Bukalapak.
+        </p>
+        <p>
+          Ketika saya memutuskan untuk meninggalkan perusahaan sebelumnya, saya mulai merapikan resume dan portofolio saya, dan mengirimkannya ke halaman <a href="https://careers.bukalapak.com/browse/Engineering" target="_blank">karirnya</a>. Tiga minggu, satu online test, serta dua wawancara yang cukup sulit, lalu <i>acceptance letter</i>, dan kemudian saya berdiri di depan pintu Bukalapak untuk memulai hari pertama dari perjalanan paling menakjubkan dari karir saya.
+        </p>
+        <p>
+          Hari pertama saya gabung, saya kaget bahwa salah satu core value di Bukalapak, yaitu <b>go the extra mile</b> benar-benar diterapkan. Saya ingat ketika di hari kedua saya baru gabung, Product manager saya menugaskan untuk memperbaiki bug di halaman pembayaran iuran premi BPJS Kesehatan. Alih-alih memperbaiki bug, senior saya menyarankan untuk sekalian merewrite kode halaman tersebut dengan teknologi yang paling baru diterapkan di Bukalapak. Saya belum pernah menulis Vue dalam hidup saya. Tetapi beberapa hari kemudian setelah kode yang saya tulis sudah berfungsi di local maupun di environment staging, PM saya berkata “Sip, deploy!”. Pada awalnya saya tidak yakin, tetapi setelah rollout ke production, saya menyadari bahwa saya baru saja mempermudah jutaan pelanggan Bukalapak untuk dapat membayar premi BPJS Kesehatannya melalui platform kami dengan mudah.
+        </p>
+        <p>
+          Ketika saya kilas balik, core value yang dimiliki Bukalapak menjadikannya sebagai tempat kerja yang luar biasa dan dinamis untuk belajar.
+        </p>
+        <h2>Gotong royong</h2>
+        <figure>
+          <img src="~/static/images/blog_contains/sebuah-refleksi-dua-tahun-di-bukalapak-1.webp" alt="Photo from Unplash">
+        </figure>
+        <p>
+          Lima bulan dari saya join, Bukalapak mengadakan hackathon untuk internal karyawannya. Tidak hanya product engineer, tapi seluruh karyawan dari berbagai divisi diperbolehkan untuk ikut. Untuk pertama kalinya, Bukalapak tidak hanya mengadakan hackathon, tapi juga testathon dan designathon. Ketiga acara tersebut diberi nama <b>Hack-a-Fun</b>.
+        </p>
+        <p>
+          Hack-a-Fun yang berlangsung selama tiga hari ini membebaskan kami dari rutinitas pekerjaan untuk berinovasi. Tujuan acara ini untuk mencari ide-ide menarik yang bisa digunakan Bukalapak. Yang lebih asiknya lagi, selama tiga hari itu terdapat banyak sekali makanan dan games-games seru yang bikin kita lebih dekat antar karyawan.
+        </p>
+        <p>
+          Saya ikut acara hackathon, dan kami bikin bot untuk pemesanan salah satu kopi kekinian melalui chat telegram.
+        </p>
+        <figure>
+          <img src="~/static/images/blog_contains/sebuah-refleksi-dua-tahun-di-bukalapak-2.webp" alt="Aplikasi bot telegram yang saya buat ketika Hack-a-Fun">
+        </figure>
+        <h2>Speak up!</h2>
+        <p>
+          Beberapa minggu setelah saya join, tim frontend Bukalapak mengadakan pertemuan rutinan tiap minggu. Acara tersebut bisa dikatakan seperti tech talks internal yang membahas mengenai teknologi frontend yang sedang berkembang.
+        </p>
+        <p>
+          Di akhir sesi acara itu, ada sesi perkenalan untuk seluruh frontend engineer yang baru bergabung di minggu tersebut.
+        </p>
+        <figure>
+          <img src="~/static/images/blog_contains/sebuah-refleksi-dua-tahun-di-bukalapak-3.webp" alt="Saya beserta FE lain yang memperkenalkan diri">
+          <figcaption>Saya beserta FE lain yang memperkenalkan diri</figcaption>
+        </figure>
+        <p>
+          Tidak hanya frontend, tiap divisi engineering juga memiliki acara talks rutinnya sendiri-sendiri. Bukalapak mendorong engineernya untuk dapat menyampaikan pendapat di depan umum, ataupun di dalam internal tim nya.
+        </p>
+        <p>
+          Tentu saja, ketika kita menyampaikan pendapat, kita tidak ngomong asal-asalan. Jauh lebih baik jika kita berbicara dengan disertai bukti berupa data atau hal lainnya sehingga diskusi atau percakapan yang terjadi bersifat konstruktif atau menuju ke arah solusi dan bukan mencari atau menyudutkan pihak yang dirasa salah.
+        </p>
+        <h2>Customer obsessed</h2>
+        <p>
+          Bukalapak hadir untuk memecahkan permasalahan yang sedang dihadapi oleh masyarakat. Apapun yang menjadi tanggungjawab kita, kita harus memiliki <i>mindset</i> bahwa apapun yang kita lakukan adalah untuk kebaikan pelanggan. Angka-angka KPI memang penting, namun memiliki empati dengan menempatkan kami sebagai pengguna startup juga tidak kalah penting.
+        </p>
+        <p>
+          Meskipun saya bekerja sebagai engineer, tetapi Bukalapak mendorong saya, dan para karyawan lainnya untuk empati kepada pelanggan kita. Pelanggan Bukalapak bisa dari mana saja. Bisa saja orang tua kita, teman kita, pasangan kita, dan orang-orang yang mengetahui bahwa kita kerja di Bukalapak. Jika ada pelanggan yang komplain ke kita mengenai transaksinya di Bukalapak, kita wajib mengekskalasi ke kanal terkait, meskipun komplain tersebut tidak ada hubungannya dengan pekerjaan kita.
+        </p>
+        <h2>Go the Extra Mile</h2>
+        <figure>
+          <img src="~/static/images/blog_contains/sebuah-refleksi-dua-tahun-di-bukalapak-4.webp" alt="Photo by Bonneval Sebastien on Usplash">
+          <figcaption>Photo by <a href="https://unsplash.com/@gentlestache" target="_blank">Bonneval Sebastien</a> on <a href="https://unsplash.com/photos/1OqbKqQ8C6I" target="_blank">Unsplash</a></figcaption>
+        </figure>
+        <p>
+          Proses pengembangan produk di sini menggunakan agile process dengan metode scrum. Awal-awal masuk Bukalapak, saya cukup roaming dengan bahasa dan istilah yang mereka pakai.
+        </p>
+        <p>
+          Ada daily standup, marketplace, retrospective, dan istilah-istilah scrum lainnya yang masih awam di telinga saya. Dari kebingungan saya pada saat itu, saya mencatat beberapa poin yang menurut saya penting.
+        </p>
+        <ul>
+          <li>
+            <b>Sprint</b> merupakan jantung dari scrum. Di Bukalapak, waktu yang digunakan untuk satu sprint adalah dua minggu.
+          </li>
+          <li>
+            Tiap awal sprint, kami melakukan <b>sprint planning</b>. Tujuannya untuk merencanakan task atau story yang akan kami kerjakan di sprint tersebut.
+          </li>
+          <li>
+            Di tiap sprint, kami menentukan <b>sprint goal</b> yang idealnya harus tercapai di sprint tersebut.
+          </li>
+          <li>
+            Di tiap sprint, kami menentukan <b>sprint goal</b> yang idealnya harus tercapai di sprint tersebut.
+          </li>
+          <li>
+            Di sepanjang sprint, kami melakukan <b>daily standup</b> meeting kurang lebih 15 menit. Meeting ini bertujuan untuk mereview task atau story yang ada di sprint tersebut.
+          </li>
+          <li>
+            Di akhir sprint, kami melakukan <b>retrospective</b>, <b>sprint review</b>, dan <b>marketplace</b>.
+          </li>
+          <li>
+            Pada sesi <b>retrospective</b>, kami meninjau performa sprint yang sudah kita lalui sebelumnya. Dari sesi ini diharapkan dapat meningkatkan performa sprint berikutnya.
+          </li>
+        </ul>
+        <h2>Try, Fail, and Try Again</h2>
+        <p>
+          Engineer Bukalapak memiliki kebebasan bereksperimen. Perusahaan memberikan rasa nyaman untuk mencoba dan mereka juga memanfaatkan itu sebaik-baiknya. Meskipun terkadang apa yang dikerjakan tidak berjalan mulus, setidaknya kami mendapatkan pelajaran dari situ.
+        </p>
+        <p>
+          Bukalapak memiliki standar yang tinggi dalam merekrut engineernya. Sehingga engineer yang masuk adalah orang-orang pilihan dan berdedikasi. Mereka berasal dari kampus-kampus ternama, tidak hanya dalam negeri, bahkan diaspora.
+        </p>
+        <figure>
+          <img src="~/static/images/blog_contains/sebuah-refleksi-dua-tahun-di-bukalapak-5.webp" alt="Saya bersama beberapa rekan kantor">
+        </figure>
+        <p>
+          <i>Best perks</i> yang dibangun oleh Bukalapak itu bukan free lunch setiap hari, flexible work hour, entertainment budget, atau bahkan gaji tinggi.
+        </p>
+        <p>
+          Tapi budaya dan kebebasan buat belajar, tidak dimarah-marahin kalau berbuat kesalahan, dikasih kepercayaan baru lulus sudah bisa bikin produk skala raksasa, dan co-workers yang berkualitas yang bisa bikin kita saling belajar satu sama lain.
+        </p>
+      </article>
+      <rg-label label="bukalapak"/>
+      <rg-label label="bukalapak engineering"/>
+      <rg-label label="culture"/>
+      <rg-label label="career"/>
+      <rg-label label="scrum"/>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Sebuah refleksi dua tahun di Bukalapak | Rizal Asrul Pambudi',
+    meta: [
+      {
+        name: 'keywords',
+        content: 'bukalapak, bukalapak engineering, culture, career, scrum, rizal, asrul, pambudi',
+      },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Tepat hari ini (20/01/2020), genap saya menginjak dua tahun di Bukalapak. Hal ini membuat saya berpikir kembali tentang semua pengalaman dan ilmu yang saya dapatkan.'
+      },
+      { property: 'og:url', content: 'https://rizalasrul.netlify.app/sebuah-refleksi-dua-tahun-di-bukalapak' },
+      {
+        property: 'og:site_name',
+        content: 'Sebuah refleksi dua tahun di Bukalapak | Rizal Asrul Pambudi',
+      },
+      {
+        property: 'og:title',
+        content: `Sebuah refleksi dua tahun di Bukalapak | Rizal Asrul Pambudi`,
+      },
+      {
+        property: 'og:description',
+        content: 'Tepat hari ini (20/01/2020), genap saya menginjak dua tahun di Bukalapak. Hal ini membuat saya berpikir kembali tentang semua pengalaman dan ilmu yang saya dapatkan.',
+      },
+      {
+        property: 'og:image',
+        content: 'https://rizalasrul.netlify.app/images/blog_thumbnails/sebuah-refleksi-dua-tahun-di-bukalapak.webp',
+      },
+    ],
+    link: [{ rel: 'canonical', href: 'https://rizalasrul.netlify.app/sebuah-refleksi-dua-tahun-di-bukalapak' }],
+  },
+};
+</script>
